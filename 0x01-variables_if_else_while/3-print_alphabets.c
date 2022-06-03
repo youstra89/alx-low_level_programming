4006857d909ca08fb19809a388fc5c317315cc0c
youstra89 <contact.youstra@gmail.com>
2022-06-03 11:05:55 -0700

    2-print_alphabet.c 3-print_alphabets.c 4-print_alphabt.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,16 @@
 int main(void)
 {
 	char letter;
+	char ch2;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		putchar(letter);
+	}
+
+	for (ch2 = 'A'; ch2 <= 'Z'; ch2++)
+	{
+		putchar(ch2);
 	}
 	putchar('\n');
 	return (0);
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,7 +12,8 @@ int main(void)
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		putchar(letter);
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
 	}
 	putchar('\n');
 	return (0);
