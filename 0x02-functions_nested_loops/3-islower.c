0393fcb8863f026b5b814ad1e65414270faf0bf9
youstra89 <contact.youstra@gmail.com>
2022-06-08 04:53:52 -0700

    5-sign.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include <ctype.h>
+#include<ctype.h>
 #include "main.h"
 
 /**
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_sign - print input number sign
+ *
+ * @n : number to check sign
+ *
+ * Return : 1 for positive number, 0 for 0 and -1 for negative number
+ */
+
+int print_sign(int n)
+{
+	int rt;
+
+	if (n == 0)
+	{
+		rt = 0;
+		_putchar(0 + '0');
+	}
+	else if (n > 0)
+	{
+		rt = 1;
+		_putchar('+');
+	}
+	else
+	{
+		rt = -1;
+		_putchar('-');
+	}
+	return (rt);
+}
