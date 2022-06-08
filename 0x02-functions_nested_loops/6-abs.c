5c1d40ef7a4bd5e00826c3c39c4590ed9e93c255
youstra89 <contact.youstra@gmail.com>
2022-06-08 12:20:56 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - print input numberabsolute value
+ *
+ * @n : number to check sign
+ *
+ * Return : absolute value
+ */
+
+int _abs(int n)
+{
+	return (abs(n));
+}
