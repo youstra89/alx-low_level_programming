ac85b30b1438cf82ac48a63a6e6eb46528c981a3
youstra89 <contact.youstra@gmail.com>
2022-06-10 07:53:49 -0700

    Edit print_remaining_days.c

--- 0x03-debugging/3-main.c
+++ 0x03-debugging/3-main.c
@@ -12,16 +12,38 @@ int main(void)
 	int month;
 	int day;
 	int year;
+	int error = 0;
 
 	month = 4;
-	day = 01;
+	day = 31;
 	year = 1997;
 
-	printf("Date: %02d/%02d/%04d\n", month, day, year);
+	if (month < 1 || month > 12)
+	{
+		error = 1;
+		printf("Invalid date");
+	}
 
-	day = convert_day(month, day);
+	if (day < 1 || day > 31)
+	{
+		error = 1;
+		print("Invalid date");
+	}
 
-	print_remaining_days(month, day, year);
+	if ((month % 2 == 0 && day == 31 && month != 8) || (year % 4 == 1 && month > 28))
+	{
+		error = 1;
+		printf("Invalid date");
+	}
+
+	if (error == 0)
+	{
+		printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+		day = convert_day(month, day);
+
+		print_remaining_days(month, day, year);
+	}
 
 	return (0);
 }
--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include "main.h"
 
-/*
- * main - prints the largest of 3 integers
+/**
+ * main - takes a date and prints how many days are left in the year, taking
+ * leap years into account
  * Return: 0
  */
 
@@ -11,15 +12,38 @@ int main(void)
 	int month;
 	int day;
 	int year;
+	int error = 0;
 
-	month = 2;
-	day = 29;
-	year = 2000;
+	month = 4;
+	day = 31;
+	year = 1997;
 
-	printf("Date: %02d/%02d/%04d\n", month, day, year);
-	day = convert_day(month, day);
+	if (month < 1 || month > 12)
+	{
+		error = 1;
+		printf("Invalid date");
+	}
 
-	print_remaining_days(month, day, year);
+	if (day < 1 || day > 31)
+	{
+		error = 1;
+		printf("Invalid date");
+	}
+
+	if ((month % 2 == 0 && day == 31 && month != 8) || (year % 4 == 1 && month > 28))
+	{
+		error = 1;
+		printf("Invalid date");
+	}
+
+	if (error == 0)
+	{
+		printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+		day = convert_day(month, day);
+
+		print_remaining_days(month, day, year);
+	}
 
 	return (0);
 }
