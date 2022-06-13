a0fc6940b4caa24c598b9ce69c82423af3fbdeda
youstra89 <contact.youstra@gmail.com>
2022-06-13 09:58:35 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - get string length
+ * @str: number to reset
+ * Return: str length
+ */
+
+int _strlen(char *str)
+{
+	return (strlen(str));
+}
