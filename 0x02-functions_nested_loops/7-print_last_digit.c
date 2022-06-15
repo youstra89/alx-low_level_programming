cbb271109ba8ff9883f69b2fb8b129341ca2b114
youstra89 <contact.youstra@gmail.com>
2022-06-15 05:18:36 -0700

    Edit print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,10 +12,20 @@
 
 int print_last_digit(int n)
 {
-	int r;
+	int last_digit;
 
-	n = abs(n);
-	r = n % 10;
-	_putchar('0' + r);
-	return (r);
+	if (n < 0)
+	{
+		last_digit = (-1 * (n % 10));
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
+
+	else
+	{
+		last_digit = (n % 10);
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
+
 }
