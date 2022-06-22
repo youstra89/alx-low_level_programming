d781d0c0d94d554cdb5220c2d95245baf020df0d
youstra89 <contact.youstra@gmail.com>
2022-06-22 14:39:45 +0000

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * @brief 
+ * _puts_recursion - function to print recursively a sentence
+ * 
+ * @s: a char to print
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
