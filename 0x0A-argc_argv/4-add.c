1c407d3b2240b5a8d4dda494de07a7973585f162
youstra89 <contact.youstra@gmail.com>
2022-06-26 10:46:24 +0000

    4-add.c and Review 3-mul.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - Program arguments number
@@ -10,16 +11,27 @@
 
 int main(int argc, char *argv[])
 {
+	int i;
 	int res;
 
-	if (argc <= 2)
+	res = 0;
+
+	if (argc <= 1)
 	{
-		printf("Error\n");
-		return (1);
+		printf("0\n");
+		return (0);
 	}
 	else
 	{
-		res = atoi(argv[1]) * atoi(argv[2]);
+		for (i = 1; i < argc; i++)
+		{
+			if((*argv[i] >= 65 && *argv[i] <= 90) || (*argv[i] >= 97 && *argv[i] <= 122))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			res = res + atoi(argv[i]);
+		}
 		printf("%d\n", res);
 		return (0);
 	}
