fc9ea362bad766fcb58c249dbb6215961b3c4aa0
youstra89 <contact.youstra@gmail.com>
2022-07-01 14:31:26 +0000

    Correct tache 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,25 +9,26 @@
 
 void print_diagonal(int n)
 {
-	int i, j;
+	int i, b;
 
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < i; j++)
+			for (b = 0; b < i; b++)
 			{
-				_putchar(' ');
-				if (j == i)
-				{
-					_putchar('\\');
-				}
+				_putchar (' ');
 			}
-			_putchar('\n');
+			_putchar (92);
+			_putchar ('\n');
+
 		}
+
 	}
+
+
 	else
 	{
-		_putchar('\n');
+		_putchar ('\n');
 	}
 }
